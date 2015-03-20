e4366f706dc0051f8e686d64ae5cf442ed16e7b1
xyw55 <xyw240497168@gmail.com>
2015-03-21 01:48:34 +0800

    add FileUtils and NodeUtils class
    1.FileUtils：静态方法mainFileFinder返回需要分析的php files。2.NodeUtils：getNodeStringName返回需要node name

--- utils/NodeUtils.class.php
+++ utils/NodeUtils.class.php
@@ -1,18 +1,77 @@
 <?php
+use PhpParser\Node;
 /**
- * NodeUtils类主要是对node节点的辅助
- * @author exploit
+ * NodeUtils类主要对node节点的辅助
+ * @author xyw55
+ *
  */
-class NodeUtils {
-	/**
-	 * 给定一个节点   根据节点的类型找出string name
-	 * @param Node $node
-	 * @return string
-	 */
-	public static function getNodeStringName($node){
-		return "" ;
-	}
-	
+class NodeUtils{
+    /**
+     * 给定一个节点，返回该节点对应string name
+     * @param Node $node
+     * @return string
+     */
+    public static function getNodeStringName(Node $node) {
+        //print_r($node);
+        $type = $node->getType();
+        //print_r($type);
+        //echo "<br/>";
+        switch ($type) {    
+            case "Expr_Variable":
+            case "Scalar_String":
+            case "Scalar_LNumber":
+            case "Scalar_DNumber":
+                $names = $node->getSubNodeNames();
+                //print_r($node->getSubNodeNames());
+                foreach ($names as $name)
+                    return($node->$name);
+                break;
+            //负数
+            case "Expr_UnaryMinus":
+                $names = $node->getSubNodeNames();
+                //print_r($node->getSubNodeNames());
+                foreach ($names as $name)
+                    return ("-".NodeUtils::getNodeStringName($node->$name));
+                break;
+            case "Name":
+                $names = $node->getSubNodeNames();
+                //print_r($node->getSubNodeNames());
+                foreach ($names as $name)
+                    foreach ($node->$name as $parts)
+                        return($parts);
+                break;
+                       
+            //$a[],$[a]$a[]][]    
+            case "Expr_ArrayDimFetch":
+                $names = $node->getSubNodeNames();
+                $a = "";
+                foreach ($names as $name)
+                {
+                    if ($name == "dim"){
+                        if ($node->$name)
+                            $a =$a. "[".NodeUtils::getNodeStringName($node->$name)."]";
+                        else 
+                            $a =$a. "[]";
+                    }
+                    else
+                        $a =$a. NodeUtils::getNodeStringName($node->$name);
+                }
+                return $a;
+                break;
+            //数组dim
+            case "Expr_ConstFetch":
+                $names = $node->getSubNodeNames();
+                //print_r($names);
+                foreach ($names as $name)
+                    return NodeUtils::getNodeStringName($node->$name);
+                break;
+                
+            default:
+                ;
+            break;
+        }
+        return "";
+    }
+    
 }
-
 ?>
