3460e7e629940fd666059cfd8e6150aedf38f056
exploit <exploitcat@foxmail.com>
2015-05-05 09:11:35 +0800

    add main.php and finish the ResultContext
    1.添加main.php来作为框架运行的主件，包括类包含和加载审计工程，以及展示模板的工作。
    
    2.完善类ResultContext
    该类主要用于记录漏洞，其中辅助类为Result，记录一条漏洞信息，包括：
    	type 漏洞类型
    	node 调用sink的节点
    	var  追踪到的最终危险的变量

--- CFGGenerator.php
+++ CFGGenerator.php
@@ -1,39 +1,6 @@
 <?php
-define('CURR_PATH',str_replace("\\", "/", dirname(__FILE__))) ;
 
-require_once CURR_PATH . '/vendor/autoload.php' ;
-require_once CURR_PATH . '/BasicBlock.php';
-
-require_once CURR_PATH . '/utils/SymbolUtils.class.php';
-require_once CURR_PATH . '/utils/NodeUtils.class.php';
-require_once CURR_PATH . '/utils/TypeUtils.class.php';
-require_once CURR_PATH . '/utils/multiBlockHandlerUtils.class.php';
-
-require_once CURR_PATH . '/symbols/Symbol.class.php' ;
-require_once CURR_PATH . '/symbols/ValueSymbol.class.php';
-require_once CURR_PATH . '/symbols/VariableSymbol.class.php';
-require_once CURR_PATH . '/symbols/MutipleSymbol.class.php';
-require_once CURR_PATH . '/symbols/ArrayDimFetchSymbol.class.php';
-require_once CURR_PATH . '/symbols/ConcatSymbol.class.php';
-require_once CURR_PATH . '/symbols/ConstantSymbol.class.php';
-require_once CURR_PATH . '/symbols/SanitizationHandler.class.php';
-require_once CURR_PATH . '/symbols/EncodingHandler.class.php';
-
-require_once CURR_PATH . '/summary/FileSummary.class.php';
-
-require_once CURR_PATH . '/context/ClassFinder.php';
-require_once CURR_PATH . '/context/UserDefinedSinkContext.class.php';
-require_once CURR_PATH . '/context/UserSanitizeFuncConetxt.php';
-
-require_once CURR_PATH . '/conf/sinks.php' ;
-require_once CURR_PATH . '/conf/sources.php' ;
-require_once CURR_PATH . '/conf/securing.php';
-
-require_once CURR_PATH . '/analyser/TaintAnalyser.class.php';
-
-header("Content-type:text/html;charset=utf-8") ;
-ini_set('xdebug.max_nesting_level', 2000);
-
+require_once CURR_PATH . './main.php';
 
 //定义PHP语句类别
 $RETURN_STATEMENT = array('Stmt_Return') ;
--- context/ResultContext.class.php
+++ context/ResultContext.class.php
@@ -1,6 +1,85 @@
 <?php
+
+/**
+ * 该上下文类用来保存report函数报告的漏洞
+ * @author Exploit
+ *
+ */
 class ResultContext {
+	//单例
+	private static $instance ;  
+	
+	private $resArr = array() ;
+	
+	/**
+	 * 添加到结果集数组中
+	 * @param Result $ele
+	 */
+	public function addResElement($ele){
+		array_push($this->resArr, $ele) ;
+	}
+	
+	
+	/**
+	 * getter
+	 */
+	public function getResArr() {
+		return $this->resArr;
+	}
+
+	//--------------------单例模式---------------------------------
+	private function __construct(){}
+
+	private function __clone(){}
+	
+	public static function getInstance(){
+		if(!(self::$instance instanceof self)){
+			self::$instance = new self ;
+		}
+		return self::$instance ;
+	}
+}
+
+
+/**
+ * 一条漏洞记录
+ * array(
+ *		'node'=> null, //调用sink的node
+ *		'var' => null, //追踪的变量node
+ *		'type' => ''   //漏洞类型
+ * ) ;
+ * 
+ * @author Exploit
+ *
+ */
+class Result{
+	private $record = array();
+	//construct
+	public function __construct($type,$node,$var){
+		$this->record = array(
+				'node'=> $node,
+				'var' => $var,
+				'type' => $type
+		) ;
+	}
+	
+	//getter
+	public function getRecord() {
+		return $this->record;
+	}
+
 	
 }
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
